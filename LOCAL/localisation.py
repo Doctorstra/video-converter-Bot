8ad58f51102091ffd9b1be660a99cfd330d35545
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-22 19:02:56 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -5,11 +5,11 @@ START_TEXT = "Send me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/Dads_links"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/Dads_links_bot"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @Doctorstra_1\n\nWritten in python library TELETHON.\n\nBot by : @Dads_links\nSupport : @Dads_links_bot\n\nV1.4"   
 
 help_text = """**v1.4**
 •`Encode` - encode your video into different lib format or resolution
@@ -22,7 +22,7 @@ help_text = """**v1.4**
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/Doctorstra_1"
 
 spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
