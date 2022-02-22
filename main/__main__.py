c0869f08734cd3677972542e3b7a006afa38be41
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-22 17:58:45 +0530

    Create __main__.py

---
+++ main/__main__.py
@@ -0,0 +1,22 @@
+import glob
+from pathlib import Path
+from main.utils import load_plugins
+import logging
+from . import Drone
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+path = "main/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+        
+print("Successfully deployed!")
+print("#MaheshChauhan • #DroneBots")
+
+if __name__ == "__main__":
+    Drone.run_until_disconnected()
