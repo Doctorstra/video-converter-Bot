fa9458eb592f91b3f3a0d84eaf937a9851f9aa6e
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-22 20:49:45 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,7 +1,15 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "𝐇𝐞𝐥𝐥𝐨! 🙋‍♂️
+
+              𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥 𝐕𝐢𝐝𝐞𝐨 𝐜𝐨𝐧𝐯𝐞𝐫𝐭𝐞𝐫 𝐁𝐨𝐭 𝐭𝐡𝐚𝐭 𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐬 𝐄𝐧𝐜𝐨𝐝𝐢𝐧𝐠 | 𝐇𝐄𝐕𝐂 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐅𝐀𝐒𝐓 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐑𝐞𝐧𝐚𝐦𝐞 | 𝐒𝐒𝐇𝐎𝐓𝐒 | 𝐓𝐫𝐢𝐦 | 𝐜𝐨𝐧𝐯𝐞𝐫 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐯𝐢𝐝𝐞𝐨,𝐯𝐢𝐝𝐞𝐨 𝐭𝐨 𝐟𝐢𝐥𝐞 | 𝐞𝐱𝐭𝐫𝐚𝐜𝐭 𝐯𝐢𝐝𝐞𝐨 𝐚𝐮𝐝𝐢𝐨)
+
+              𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧 𝐔𝐩𝐥𝐨𝐚𝐝 𝐕𝐢𝐝𝐞𝐨/𝐅𝐢𝐥𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐰𝐢𝐭𝐡 𝐀𝐝𝐝𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.
+
+              𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐥𝐩 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬!
+
+              𝐌𝐚𝐝𝐞 𝐰𝐢𝐭𝐡 𝐛𝐲 ❤️ @Dads_links 𝗔𝗱𝗺𝗶𝗻."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
@@ -12,13 +20,20 @@ SUPPORT_LINK = "https://t.me/Dads_links_bot"
 info_text = "This bot is developed by @Doctorstra_1\n\nWritten in python library TELETHON.\n\nBot by : @Dads_links\nSupport : @Dads_links_bot\n\nV1.4"   
 
 help_text = """**v1.4**
-•`Encode` - encode your video into different lib format or resolution
-•`HEVC compress` - negligible loss compression
-•`FAST compress` - Very fast and Efficient compression
-•`Convert` - change formats or extract audio of any video
-•`Rename` - rename any file, extension not required
-•`SSHOTS` - generate 10 screenshots of your video
-•`Trim` - cut your videos
+💎`Encode` - encode your video into different lib format or resolution
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+💎`HEVC compress` - negligible loss compression
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+💎`FAST compress` - Very fast and Efficient compression
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+💎`Convert` - change formats or extract audio of any video
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+💎`Rename` - rename any file, extension not required
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+💎`SSHOTS` - generate 10 screenshots of your video
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+💎`Trim` - cut your videos
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 
 Made With by ❤️ @Dads_links 𝗔𝗱𝗺𝗶𝗻"""
 
