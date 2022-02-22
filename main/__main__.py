9300ac7ee5f76dc4126a26eae402417abaed4f4b
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-22 19:14:10 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
         
 print("Successfully deployed!")
-print("#MaheshChauhan • #DroneBots")
+print("#Doctorstra • #Dads_links_bot")
 
 if __name__ == "__main__":
     Drone.run_until_disconnected()
