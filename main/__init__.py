5dc706641a2ebeeff960d276e44a48e09c5778ba
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-02-22 17:57:52 +0530

    Create __init__.py

---
+++ main/__init__.py
@@ -0,0 +1,24 @@
+#ChauhanMahesh/DroneBots/COL
+
+from telethon import TelegramClient
+from decouple import config
+import logging
+import time
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# variables
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+BOT_UN = config("BOT_UN", default=None)
+AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
+LOG_CHANNEL = config("LOG_CHANNEL", default=None)
+LOG_ID = config("LOG_ID", default=None)
+FORCESUB = config("FORCESUB", default=None)
+FORCESUB_UN = config("FORCESUB_UN", default=None)
+ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
+MONGODB_URI = config("MONGODB_URI", default=None)
+
+Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
