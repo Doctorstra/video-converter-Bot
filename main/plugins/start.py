5e9068b5a501ec33b66997d8c7141a30c24ac748
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-03-01 19:22:22 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -50,7 +50,7 @@ async def source(event):
                     
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
 async def help(event):
-    await event.edit('**👥HELP & SETTINGS**\n\n〽️ Powered by @Dads_links',
+    await event.edit('{START_TEXT}',
                     buttons=[[
                          Button.inline("SET THUMB 🌆", data="sett"),
                          Button.inline("DEL THUMB 🗑️", data='remt')],
