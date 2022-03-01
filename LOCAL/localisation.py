738fc633884b86051d4bfce5842a80ee24e83d37
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-03-01 19:06:08 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -3,6 +3,12 @@
 
 START_TEXT = "𝐇𝐞𝐥𝐥𝐨 🙋‍♂️{}\n\n𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥 𝐕𝐢𝐝𝐞𝐨 𝐜𝐨𝐧𝐯𝐞𝐫𝐭𝐞𝐫 𝐁𝐨𝐭 𝐭𝐡𝐚𝐭 𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐬 𝐄𝐧𝐜𝐨𝐝𝐢𝐧𝐠 | 𝐇𝐄𝐕𝐂 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐅𝐀𝐒𝐓 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐑𝐞𝐧𝐚𝐦𝐞 | 𝐒𝐒𝐇𝐎𝐓𝐒 | 𝐓𝐫𝐢𝐦 | 𝐜𝐨𝐧𝐯𝐞𝐫 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐯𝐢𝐝𝐞𝐨,𝐯𝐢𝐝𝐞𝐨 𝐭𝐨 𝐟𝐢𝐥𝐞 | 𝐞𝐱𝐭𝐫𝐚𝐜𝐭 𝐯𝐢𝐝𝐞𝐨 𝐚𝐮𝐝𝐢𝐨)\n\n𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧 𝐔𝐩𝐥𝐨𝐚𝐝 𝐕𝐢𝐝𝐞𝐨/𝐅𝐢𝐥𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐰𝐢𝐭𝐡 𝐀𝐝𝐝𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.\n\n𝐂𝐥𝐢𝐜𝐤 /help 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬!\n\n𝐌𝐚𝐝𝐞 𝐰𝐢𝐭𝐡 𝐛𝐲 ❤️ @Dads_links 𝗔𝗱𝗺𝗶𝗻."
 
+FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
+
+CHANNEL_LINK = "https://t.me/Dads_links"
+
+SUPPORT_LINK = "https://t.me/Dads_links_bot" 
+
 info_text = """**About me** 😉\n\n
 ✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: DADS LINKS VIDEO CONVERTER BOT 
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Dads_links>Dads Links</a>
@@ -10,13 +16,7 @@ info_text = """**About me** 😉\n\n
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-
-FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
-
-CHANNEL_LINK = "https://t.me/Dads_links"
-
-SUPPORT_LINK = "https://t.me/Dads_links_bot"  
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]""" 
 
 help_text = """**DADS LINKS VIDEO CONVERTER**
 💎`Encode` - encode your video into different lib format or resolution
