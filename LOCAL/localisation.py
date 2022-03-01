05292e3bc303a9bbb9df2dc6fdbd0e38d259fd0a
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2022-03-01 23:45:04 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,7 +1,7 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "𝐇𝐞𝐥𝐥𝐨! 🙋‍♂️\n\n𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥 𝐕𝐢𝐝𝐞𝐨 𝐜𝐨𝐧𝐯𝐞𝐫𝐭𝐞𝐫 𝐁𝐨𝐭 𝐭𝐡𝐚𝐭 𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐬 𝐄𝐧𝐜𝐨𝐝𝐢𝐧𝐠 | 𝐇𝐄𝐕𝐂 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐅𝐀𝐒𝐓 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐑𝐞𝐧𝐚𝐦𝐞 | 𝐒𝐒𝐇𝐎𝐓𝐒 | 𝐓𝐫𝐢𝐦 | 𝐜𝐨𝐧𝐯𝐞𝐫 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐯𝐢𝐝𝐞𝐨,𝐯𝐢𝐝𝐞𝐨 𝐭𝐨 𝐟𝐢𝐥𝐞 | 𝐞𝐱𝐭𝐫𝐚𝐜𝐭 𝐯𝐢𝐝𝐞𝐨 𝐚𝐮𝐝𝐢𝐨)\n\n𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧 𝐔𝐩𝐥𝐨𝐚𝐝 𝐕𝐢𝐝𝐞𝐨/𝐅𝐢𝐥𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐰𝐢𝐭𝐡 𝐀𝐝𝐝𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.\n\n𝐂𝐥𝐢𝐜𝐤 /help 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬!\n\n𝐌𝐚𝐝𝐞 𝐰𝐢𝐭𝐡 𝐛𝐲 ❤️ @Dads_links 𝗔𝗱𝗺𝗶𝗻."
+START_TEXT = "𝐇𝐞𝐥𝐥𝐨! 🙋‍♂️\n\n𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐟𝐮𝐥 𝐕𝐢𝐝𝐞𝐨 𝐜𝐨𝐧𝐯𝐞𝐫𝐭𝐞𝐫 𝐁𝐨𝐭 𝐭𝐡𝐚𝐭 𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐬 𝐄𝐧𝐜𝐨𝐝𝐢𝐧𝐠 | 𝐇𝐄𝐕𝐂 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐅𝐀𝐒𝐓 𝐜𝐨𝐦𝐩𝐫𝐞𝐬𝐬 | 𝐑𝐞𝐧𝐚𝐦𝐞 | 𝐒𝐒𝐇𝐎𝐓𝐒 | 𝐓𝐫𝐢𝐦 | 𝐜𝐨𝐧𝐯𝐞𝐫 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐯𝐢𝐝𝐞𝐨,𝐯𝐢𝐝𝐞𝐨 𝐭𝐨 𝐟𝐢𝐥𝐞 | 𝐞𝐱𝐭𝐫𝐚𝐜𝐭 𝐯𝐢𝐝𝐞𝐨 𝐚𝐮𝐝𝐢𝐨)\n\n𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧 𝐔𝐩𝐥𝐨𝐚𝐝 𝐕𝐢𝐝𝐞𝐨/𝐅𝐢𝐥𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐰𝐢𝐭𝐡 𝐀𝐝𝐝𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.\n\n𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 🙎 : @Doctorstra_1\n\n𝐌𝐚𝐝𝐞 𝐰𝐢𝐭𝐡 𝐛𝐲 ❤️ @Dads_links 𝗔𝗱𝗺𝗶𝗻."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
@@ -10,16 +10,16 @@ CHANNEL_LINK = "https://t.me/Dads_links"
 SUPPORT_LINK = "https://t.me/Dads_links_bot" 
 
 info_text = """**About me** 😉\n\n
-<b>🤖 My Name: <a href='https://t.me/Dads_links_VIDEO_CONVERTER_bot'> DADS LINKS VIDEO CONVERTER BOT </a></b>
-<b>👨‍💻 Developer :<a href='https://t.me/Doctorstra_1'>DOCTORSTRA</a></b>
-<b>📝 Language: 𝐏𝐲𝐭𝐡𝐨𝐧𝟑</b>
-<b>📡 Server: <a href='https://www.heroku.com'> Heroku </a></b>
-<b>📕 Library: Pyrogram 1.0.7</b>
-<b>💢 Source Code:<a href='https://t.me/Doctorstra_1'> Click Here </a></b>
-<b>📢 CHANNEL:<a href='https://t.me/Dads_links'> DADS LINKS</a></b>
-<b>🔔 Bot CHANNEL: <a href='https://t.me/Dads_links_bot'> DADS LINKS BOT</a></b>
-<b>🌀 Telegram BotList: <a href='https://t.me/Dads_links/32'> DADS LINKS BOT LIST</a></b>
-<b>🚸 Powered By: <a href='https://t.me/Dads_links'> DADS LINKS</a></b>"""
+🤖 My Name         : @Dads_links_VIDEO_CONVERTER_bot
+👨‍💻 Developer       : @Doctorstra_1
+📝 Language        : 𝐏𝐲𝐭𝐡𝐨𝐧𝟑
+📡 Server          : Heroku
+📕 Library         : Pyrogram 1.0.7
+💢 Source Code     : @Doctorstra_1
+📢 CHANNEL         : @Dads_links
+🔔 Bot CHANNEL     : @Dads_links_bot
+🌀 Telegram BotList: @Dads_links/32
+🚸 Powered By      : @Dads_links
 
 help_text = """**DADS LINKS VIDEO CONVERTER**
 💎`Encode` - encode your video into different lib format or resolution
